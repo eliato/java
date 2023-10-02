327eead66e817be801ef5c9f6269f9f5341a5767
Elias Torres <eliato_@hotmail.com>
2023-10-01 21:28:06 -0600

    anotation RequestMapping

--- spring-boot-web/src/main/java/com/example/springbootweb/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -3,9 +3,11 @@ package com.example.springbootweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     //@RequestMapping(value = "/index", method = RequestMethod.GET)
