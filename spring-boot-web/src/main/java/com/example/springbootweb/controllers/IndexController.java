c06b0c450eb249e289358f6e0f85b8da098a7aba
Elias Torres <eliato_@hotmail.com>
2023-10-01 20:40:11 -0600

    init project java

---
+++ spring-boot-web/src/main/java/com/example/springbootweb/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    //@RequestMapping(value = "/index", method = RequestMethod.GET)
+    @GetMapping({"/index", "/", "/home"})
+    public String index(){
+        return "index";
+    }
+
+}
