b188e0990da2a0eeb33172a1251019bcb5e4b64f
Elias Torres <eliato_@hotmail.com>
2023-10-04 18:59:42 -0600

    param for default

---
+++ spring-boot-web/src/main/java/com/example/springbootweb/EjemploParamsController.java
@@ -0,0 +1,20 @@
+package com.example.springbootweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class EjemploParamsController {
+
+    @GetMapping("/string")
+    public String param(@RequestParam(name = "texto", required = false, defaultValue = "Algún valor") String texto, Model model){
+        model.addAttribute("titulo", "Recibir parametros del Request HTTP GET- URL");
+        model.addAttribute("resultado", "El txto enviado es:" + texto);
+        return "params/ver";
+    }
+
+}
